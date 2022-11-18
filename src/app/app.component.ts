cd57464d7ed522140797a5d474dd3faa5c189521
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2022-11-18 23:28:14 +0530

    feat injected functionalities in the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstAngularProject';
+  todoValue: string;
+  list: Todo[];
+  ngOnInit() {
+    this.todoValue = '';
+    this.list = [];
+  }
+  addItem() {
+    if (this.todoValue !== '') {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isCompleted: false,
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = '';
+  }
+  deleteItem(id: number) {
+    this.list = this.list.filter((item) => item.id !== id);
+  }
 }
